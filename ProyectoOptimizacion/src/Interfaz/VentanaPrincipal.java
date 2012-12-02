03070122dd5a4c4f21a3d0776d10e4425e5810dc
themonki <edgarandres29@gmail.com>
2012-12-02 17:54:14 -0500

    actualizando git ignore y declarando algunas clases en ventanaprincipal

--- ProyectoOptimizacion/src/Interfaz/VentanaPrincipal.java
+++ ProyectoOptimizacion/src/Interfaz/VentanaPrincipal.java
@@ -2,6 +2,8 @@
 package Interfaz;
 
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 /**
@@ -9,7 +11,9 @@ import javax.swing.JFrame;
  * @author Edgar Andrés Moncada
  */
 public class VentanaPrincipal extends JFrame {
-
+    
+    Manejador manejador;
+    
     public VentanaPrincipal() throws HeadlessException {
         
         super();
@@ -30,6 +34,25 @@ public class VentanaPrincipal extends JFrame {
         
     }
     
+    public void agregarEventos(){
+        
+    }
+    
+    
+    private class Manejador implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            
+            if(ae.getSource()==""){
+            
+            }else if(ae.getSource()=="."){
+            }
+            
+        }
+    
+    
+    }
     
     
 }
